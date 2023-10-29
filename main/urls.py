2de2d37590427d187d30d3f7200ba221286bfe36
keishabella <anastasiakeishaa@gmail.com>
2023-10-29 18:03:56 +0700

    added favorite books

--- main/urls.py
+++ main/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('user_registered/', include('user_registered.urls')),
-    
 ]
