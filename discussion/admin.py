f9b1712cf47e2c00ad69de2068bce50948f45eb0
tegar <tegar.wahyu@ui.ac.id>
2023-10-29 23:38:26 +0700

    feat: penambahan manage model Discussion di admin

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from discussion.models import DiscussionThread, DiscussionReply
 
 # Register your models here.
+admin.site.register(DiscussionThread)
+admin.site.register(DiscussionReply)
