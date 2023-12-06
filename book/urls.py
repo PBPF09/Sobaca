479d858a4a2376f163b41fdb4e22ccf59030f3d9
tajreee <tajrianhaar@gmail.com>
2023-12-06 15:33:04 +0700

    add new views

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from book.views import get_books
+from book.views import get_books, get_books_by_typing, get_books_by_genre
 
 app_name = "book"
 
 urlpatterns = [
-    path("", get_books, name="get_books")
+    path("", get_books, name="get_books"),
+    path("<str:typing>", get_books_by_typing, name="get_books_by_typing"),
+    path("genres/<str:genre>", get_books_by_genre, name="get_books_by_genre")
 ]
