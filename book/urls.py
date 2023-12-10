b80c6055fce4c8c563aefae74349d45530be29e5
tajreee <tajrianhaar@gmail.com>
2023-12-10 16:23:28 +0700

    update views.py

--- book/urls.py
+++ book/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from book.views import get_books, get_books_by_typing, get_books_by_genre
+from book.views import get_books, get_books_by_typing, get_books_by_genre, get_books_by_typing
 
 app_name = "book"
 
 urlpatterns = [
     path("", get_books, name="get_books"),
     path("<str:typing>", get_books_by_typing, name="get_books_by_typing"),
-    path("genres/<str:genre>", get_books_by_genre, name="get_books_by_genre")
+    path("genres/<str:genre>", get_books_by_genre, name="get_books_by_genre"),
 ]
