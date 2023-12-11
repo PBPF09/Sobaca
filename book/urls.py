8434e4b60cea3176b34b80968e84c4e418050252
tajreee <tajrianhaar@gmail.com>
2023-12-11 16:13:25 +0700

    update book\views.py

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from book.views import get_books, get_books_by_typing, get_books_by_genre, get_books_by_typing
+from book.views import get_books, get_books_by_typing, get_books_by_genre, get_books_by_typing, order_books
 
 app_name = "book"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", get_books, name="get_books"),
     path("<str:typing>", get_books_by_typing, name="get_books_by_typing"),
     path("genres/<str:genre>", get_books_by_genre, name="get_books_by_genre"),
+    path("order-by/<str:order>/", order_books, name="order_books")
 ]
--- book/views.py
+++ book/views.py
@@ -9,7 +9,7 @@ def get_books(request):
     data = Book.objects.all().order_by('?')
     return HttpResponse(serializers.serialize("json", data), content_type="application/json") 
 
-@login_required(login_url='/login')
+# @login_required(login_url='/login')
 def get_books_by_typing(request, typing):
     if len(typing) == 0:
         books = Book.objects.filter(
@@ -23,7 +23,7 @@ def get_books_by_typing(request, typing):
     
     return HttpResponse(serializers.serialize("json", books), content_type="application/json")
 
-@login_required(login_url='/login')
+# @login_required(login_url='/login')
 def get_books_by_genre(request, genre):
     if genre == "All":
         books = Book.objects.all().order_by('?')
@@ -32,3 +32,16 @@ def get_books_by_genre(request, genre):
         books = Book.objects.filter(categories__contains=genre).order_by('title')
     
     return HttpResponse(serializers.serialize("json", books), content_type="application/json")
+
+# @login_required(login_url='/login')
+def order_books(request, order):
+    if order == "ascending":
+        # print(order)
+        books = Book.objects.all().order_by('title')
+    
+    elif order == "descending":
+        # print(order)
+        books = Book.objects.all().order_by('-title')
+    
+    return HttpResponse(serializers.serialize("json", books), content_type="application/json")
+
